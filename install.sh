b4e646dba76fd29b098aacbfa709a119fa01512e
Osenberg-Y <osenberg@qq.com>
2018-07-23 20:15:02 +0800

    vim-plug vim-airline YCM

--- install.sh
+++ install.sh
@@ -4,5 +4,18 @@ Dir=$HOME/.PersonVim
 mkdir $Dir
 cd $Dir
 git clone https://github.com/vim/vim.git
-cd vim && ./configure && make -j `nproc` && make install
+cd vim
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp \
+            --enable-pythoninterp \
+            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+            --enable-perlinterp \
+            --enable-luainterp \
+            --enable-gui=gtk2
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim81
+sudo make install
 ln -s $Dir/config/vimrc $HOME/.vimrc
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
